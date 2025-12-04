ec6de9f4ff6bbdfdb24586ec74625494eac03ef8
sivasai187 <shivasai1872005115@gmail.com>
2025-12-04 22:24:24 +0530

    custom types done

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
-  type Query {
+  type Post {
     id:ID!
-    name:String
-    age:Int
-    isActive:Boolean
-    height:Float
+    title:String!
+    body:String!
+    tags:[String]
+    
+  }
+  type Query {
+    posts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    id:()=>"1",
-    name:()=>"siva sai",
-    age:()=>20,
-    isActive:()=>true,
+    posts: () =>[
+     {
+        id:"1",
+        title:"Post 1",
+        body:"Body of post 1",
+        tags:["tag1","tag2"],
+     },
+     {
+        id:"2",
+        title:"Post 2",
+        body:"Body of post 2",
+        tags:["tag3","tag4"],
+     }
+    ]
   },
+
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
